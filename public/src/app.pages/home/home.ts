034decc8021d7df736089b665533cac4512e91d0
Julien Avert <julien.avert@viseo.com>
2017-03-23 22:18:08 +0100

    added a dummy controller to home page & slightly tweaked contacts style

--- public/src/app.pages/home/home.ts
+++ public/src/app.pages/home/home.ts
@@ -1,12 +1,25 @@
 module app.pages {
   'use strict';
 
+  class HomeCtrl {
+
+    constructor(
+      private $scope: ng.IScope
+    ) {
+      this.text = 'Lolilol';
+    }
+
+    private text: string;
+  }
+
   angular
     .module('app.pages')
     .config(($stateProvider: ng.ui.IStateProvider) => {
       $stateProvider.state('shell.home', {
         url: '/home',
-        templateUrl: 'app.pages/home/home.html'
+        templateUrl: 'app.pages/home/home.html',
+        controller: HomeCtrl,
+        controllerAs: 'vm'
       });
     });
 }
