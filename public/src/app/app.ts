217d76708e1ce6f3fe28b98d45fcd8c5bc1135f5
Julien Avert <julien.avert@viseo.com>
2017-04-10 23:48:32 +0200

    added app.config.ts and moved code around

--- public/src/app/app.ts
+++ public/src/app/app.ts
@@ -9,19 +9,4 @@ angular
     'app.pages',
     'app.directives',
     'app.services'
-  ])
-  .config((
-    $urlRouterProvider: ng.ui.IUrlRouterProvider,
-    $translateProvider: ng.translate.ITranslateProvider
-  ) => {
-    // default router route
-    $urlRouterProvider.otherwise('/home');
-    // translation config
-    $translateProvider.useStaticFilesLoader({
-      prefix: 'i18n/',
-      suffix: '.json'
-    });
-
-    // $translateProvider.preferredLanguage('en_US');
-    $translateProvider.preferredLanguage('fr_FR');
-  });
+  ]);
